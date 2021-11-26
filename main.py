b7172480fab1a5b52e589b5e717f4efed383b5ee
Broken_Rake <wffpiter@gmail.com>
2021-11-26 23:33:14 +0300

    Капучино

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sys
 from PyQt5 import uic, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
     
 
 class MainWindow(QMainWindow):
@@ -10,8 +10,15 @@ class MainWindow(QMainWindow):
         uic.loadUi('main.ui', self)
         self.setWindowTitle('CoffeeTable')
 
+        update = self.output()
+
+        self.Button_recording.clicked.connect(self.recording)
+        self.Button_change.clicked.connect(self.change)
+
+    def output(self):
         con = sqlite3.connect('coffee.sqlite')
         res = con.cursor().execute("""SELECT * FROM coffee""").fetchall()
+        con.close()
 
         self.TableTitle = ['ID', 'название сорта', 'степень обжарки,', 'молотый/в зернах',
                            'описание вкуса', 'цена', ' объем упаковки']
@@ -24,6 +31,71 @@ class MainWindow(QMainWindow):
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(
                     i, j, QtWidgets.QTableWidgetItem(str(elem)))
+
+    def recording(self):
+        recording = Recording()
+        recording.exec_()
+        update = self.output()
+
+    def change(self):
+        change = Change()
+        change.exec_()
+        update = self.output()
+
+
+class Recording(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setWindowTitle('Recording')
+        self.pushButton.setText('Добавить')
+
+        self.id_edit.hide()
+        self.label_id.hide()
+
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        VarietyName, RoastDegree, GroundOrBeans, FlavorDescription, \
+        price, volume = self.name_edit.text(), self.RoastDegree_edit.text(), \
+                        self.GroundOrBeans_edit.text(), self.Description_Edit.toPlainText(), \
+                        self.price_edit.text(), self.volume_edit.text()
+
+        con = sqlite3.connect('coffee.sqlite')
+        res = con.cursor().execute("""INSERT INTO coffee(VarietyName, RoastDegree, GroundOrBeans,
+                                                            FlavorDescription, price, volume)
+                                    VALUES(?, ?, ?, ?, ?, ?)""", (VarietyName, RoastDegree, GroundOrBeans,
+                                                                  FlavorDescription, price, volume))
+        con.commit()
+        con.close()
+        self.msg.setText('Добавлено')
+
+
+class Change(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setWindowTitle('Change')
+        self.pushButton.setText('Изменить')
+
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        ID, VarietyName, RoastDegree, GroundOrBeans, FlavorDescription, \
+        price, volume = self.id_edit.text(), self.name_edit.text(), self.RoastDegree_edit.text(), \
+                        self.GroundOrBeans_edit.text(), self.Description_Edit.toPlainText(), \
+                        self.price_edit.text(), self.volume_edit.text()
+
+        con = sqlite3.connect('coffee.sqlite')
+        res = con.cursor().execute("""UPDATE coffee
+                                    SET VarietyName = ?,RoastDegree = ?, GroundOrBeans = ?, 
+                                    FlavorDescription = ?, price = ?, volume = ?
+                                    WHERE id = {}""".format(ID),
+                                   (VarietyName, RoastDegree, GroundOrBeans,
+                                    FlavorDescription, price, volume))
+        con.commit()
+        con.close()
+        self.msg.setText('Изменино')
 
 
 if __name__ == '__main__':
